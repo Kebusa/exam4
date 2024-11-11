75d5a6dad38518078d74948db4fb72bd7f9da5c4
Asubek <asubekerakyluly@gmail.com>
2024-11-11 11:18:07 +0500

    task 1

---
+++ script.js
@@ -0,0 +1,19 @@
+var num = Number(prompt("Ай санын енгізіңіз: "))
+
+if((num == 3) || (num == 4)|| (num == 5)){
+    alert("Көктем")
+}
+else if( (num == 6) || (num == 7) || (num == 8)){
+    alert("Жаз")
+}
+else if( (num == 9) || (num == 10) || (num == 11)){
+    alert("Күз")
+}
+else if( (num == 12) || (num == 1) || (num == 2)){
+    alert("Қыс")
+}
+else{
+    alert("1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз")
+}
+
+
