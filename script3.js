a98af1c0714755dbfcaf941eecbaa1aaafe731bc
Asubek <asubekerakyluly@gmail.com>
2024-11-11 11:56:39 +0500

    task 3 final form

---
+++ script3.js
@@ -0,0 +1,14 @@
+var a = Number(prompt(""))
+var b = Number(prompt(""))
+
+if(a > b){
+    alert("a саны b санынан үлкен")
+}
+else if(b > a){
+    alert("b саны a санынан үлкен")
+}
+else if(a == b){
+    alert("a саны b санымен тең")
+}
+
+alert("а және b санының орташасы: " + ((a + b) / 2))
